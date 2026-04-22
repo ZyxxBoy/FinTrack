ec4902510d1d81a28a609da1506139964a5af912
ZyxxBoy <m.nauvalhakim123@gmail.com>
2026-04-22 08:34:49 +0700

    chore: fix vite chunk size warning

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
+  build: {
+    chunkSizeWarningLimit: 3000, // Menghilangkan peringatan untuk file di bawah 3MB
   }
 })
